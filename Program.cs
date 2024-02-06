3bd19b787a01cfd5360de651989840622f263dec
Lucan <Lucan@lucanops.net>
2024-02-06 00:35:25 +0000

    Removed unneeded SelfHost Code

--- Program.cs
+++ Program.cs
@@ -143,54 +143,7 @@ var query2 = ClassesList.SelectMany(c => c.Element).Where(cr => cr.Name == "Bard
 int test;
 test = 1;
 
-namespace SelfHost
-{
 
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Net;
-    using System.Web.Http;
-
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public decimal Price { get; set; }
-    }
-
-    public class ProductsController : ApiController
-    {
-        Product[] products = new Product[]
-        {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
-        };
-
-        public IEnumerable<Product> GetAllProducts()
-        {
-            return products;
-        }
-
-        public Product GetProductById(int id)
-        {
-            var product = products.FirstOrDefault((p) => p.Id == id);
-            if (product == null)
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
-            return product;
-        }
-
-        public IEnumerable<Product> GetProductsByCategory(string category)
-        {
-            return products.Where(p => string.Equals(p.Category, category,
-                    StringComparison.OrdinalIgnoreCase));
-        }
-    }
-}
 namespace Items
 {
     [XmlRoot(ElementName = "description")]
